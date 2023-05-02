903bf00d11c06047f465887075845a1b94467af3
C. Felix Lee <felixlee.music@gmail.com>
2023-05-02 17:02:24 -0400

    nearly done, still need to show error messages

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,22 +1,17 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-export default function DarkMode () {
-  const [darkMode, setDarkMode] = useState(false)
+export default function DarkMode() {
+  const [darkMode, setDarkMode] = useState(false);
 
   return (
-    <div className={`page ${darkMode && 'dark-mode'}`}>
-      <button
-        className='dark-mode-button'
-        onClick={() => setDarkMode(true)}
-      >
+    //uses a backticks & a JS expression to dynamically change the element's class name
+    <div className={`page ${darkMode && "dark-mode"}`}>
+      <button className='dark-mode-button' onClick={() => setDarkMode(true)}>
         Dark Mode
       </button>
-      <button
-        className='light-mode-button'
-        onClick={() => setDarkMode(false)}
-      >
+      <button className='light-mode-button' onClick={() => setDarkMode(false)}>
         Light Mode
       </button>
     </div>
-  )
+  );
 }
--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -1,29 +1,61 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-export default function FormValidator () {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordConfirm, setPasswordConfirm] = useState('')
+export default function FormValidator() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [message, setMessage] = useState("");
 
+  const findErrors = () => {
+    const errors = [];
+
+    //check for each field
+    if (!email || !password || !passwordConfirm)
+      errors.push("All fields must be filled in");
+    if ([...email].filter((i) => i === "@").length !== 1) {
+      errors.push("An email must have exactly one @ symbol");
+    }
+    if (password.length > 8)
+      errors.push("Password must be at least 8 characters long");
+    if (passwordConfirm !== password) errors.push("Passwords must match");
+
+    return errors;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const errors = findErrors();
+    setMessage(errors.length ? errors.join(",") : "Success!");
+    console.log(message);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
       <input
-        type='text' name='email'
-        onChange={e => setEmail(e.target.value)}
+        required
+        type='text'
+        name='email'
+        onChange={(e) => setEmail(e.target.value)}
       />
       <label htmlFor='password'>Password</label>
       <input
-        type='password' name='password'
-        onChange={e => setPassword(e.target.value)}
+        required
+        minLength={8}
+        type='password'
+        name='password'
+        onChange={(e) => setPassword(e.target.value)}
       />
       <label htmlFor='password-confirm'>Confirm Password </label>
       <input
-        type='password' name='password-confirm'
-        onChange={e => setPasswordConfirm(e.target.value)}
+        required
+        type='password'
+        name='password-confirm'
+        onChange={(e) => setPasswordConfirm(e.target.value)}
       />
-      <input type='submit' value='Submit' />
+      <p id='validation_msg'></p>
+      <input type='submit' id='submit' value='Submit' />
     </form>
-  )
+  );
 }
