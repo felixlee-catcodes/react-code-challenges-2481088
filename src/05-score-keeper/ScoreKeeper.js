28e71cd690cce47ae371e24f084b312b1407b67c
C. Felix Lee <felixlee.music@gmail.com>
2023-05-04 19:20:30 -0400

    exercise complete, added a button to reset the score

--- src/05-score-keeper/ScoreKeeper.js
+++ src/05-score-keeper/ScoreKeeper.js
@@ -1,13 +1,23 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-export default function ScoreKeeper () {
-  const [score, setScore] = useState(0)
+export default function ScoreKeeper() {
+  //useState grabs the score stored/persisting in local storage and uses that as its inital state
+  const [score, setScore] = useState(
+    //if no score is being stored in local storage, then display 0 as the score
+    parseInt(localStorage.getItem("score")) || 0
+  );
+
+  useEffect(() => {
+    localStorage.setItem("score", score);
+    console.log("current score: ", score);
+  }, [score]);
 
   return (
     <div>
       <h1>Your score is: {score}</h1>
-      <button onClick={() => setScore(prevScore => prevScore + 1)}>+</button>
-      <button onClick={() => setScore(prevScore => prevScore - 1)}>-</button>
+      <button onClick={() => setScore((prevScore) => prevScore + 1)}>+</button>
+      <button onClick={() => setScore((prevScore) => prevScore - 1)}>-</button>
+      <button onClick={() => setScore(0)}>Reset Score</button>
     </div>
-  )
+  );
 }
