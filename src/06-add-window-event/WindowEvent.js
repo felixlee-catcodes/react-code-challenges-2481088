8ffd11671c583f46159089e433baf850e6d93550
C. Felix Lee <felixlee.music@gmail.com>
2023-05-07 00:58:15 -0400

    exercise completed

--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,5 +1,13 @@
-export default function WindowEvent () {
-  return (
-    <h2>Window event active</h2>
-  )
+import { useEffect } from "react";
+
+export default function WindowEvent() {
+  useEffect(() => {
+    const doubleClick = () => alert("mouse clicked!");
+
+    window.addEventListener("dblclick", doubleClick);
+
+    //remove event listener by having useEffect return a function (cool!)
+    return () => window.removeEventListener("dblclick", doubleClick);
+  }, []);
+  return <h2>Window event active</h2>;
 }
