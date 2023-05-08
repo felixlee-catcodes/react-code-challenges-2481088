9f288f8c06dafbdffaa73863386114943f93115b
C. Felix Lee <felixlee.music@gmail.com>
2023-05-08 17:11:23 -0400

    exercise complete, plan to add additional functionality such as erase

--- src/08-pixel-art/PixelArt.js
+++ src/08-pixel-art/PixelArt.js
@@ -1,34 +1,70 @@
-import React from 'react'
+import React, { createContext, useContext, useState } from "react";
 
-function ColorPicker () {
-  const colors = ['red', 'blue', 'yellow', 'green', 'black', 'white', 'purple']
+const ColorContext = createContext({
+  color: "lightGrey",
+  setColor: () => {},
+});
+
+function ColorPicker() {
+  const colors = ["red", "blue", "yellow", "green", "black", "white", "purple"];
+  const { setColor } = useContext(ColorContext);
   return (
     <div>
       <h1>Choose a color</h1>
-      {colors.map(color => <button key={color} style={{ backgroundColor: color }} />)}
+      {colors.map((color) => (
+        <button
+          key={color}
+          style={{ backgroundColor: color, border: "1px solid grey" }}
+          onClick={() => {
+            setColor(color);
+            console.log("color: " + color);
+          }}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-function Pixel () {
-  return <div style={{ height: '20px', width: '20px', backgroundColor: 'lightGrey', margin: '1px' }} />
-}
-
-function Pixels () {
-  const pixels = []
-  for (let i = 0; i < 100; i++) pixels.push(<Pixel key={i} />)
+function Pixel() {
+  const { color } = useContext(ColorContext);
+  const [pixelColor, setPixelColor] = useState("lightGrey");
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(10, 1fr)', width: '210px', margin: '0 auto' }}>
+    <div
+      onClick={() => setPixelColor(color)}
+      style={{
+        height: "20px",
+        width: "20px",
+        backgroundColor: pixelColor,
+        margin: "1px",
+      }}
+    />
+  );
+}
+
+function Pixels() {
+  const pixels = [];
+  for (let i = 0; i < 100; i++) pixels.push(<Pixel key={i} />);
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(10, 1fr)",
+        width: "210px",
+        margin: "0 auto",
+      }}
+    >
       {pixels}
     </div>
-  )
+  );
 }
 
-export default function PixelArt () {
+export default function PixelArt() {
+  //set up state for top level component of the app
+  const [color, setColor] = useState("lightGrey");
   return (
-    <div>
+    <ColorContext.Provider value={{ color, setColor }}>
       <ColorPicker />
       <Pixels />
-    </div>
-  )
+    </ColorContext.Provider>
+  );
 }
